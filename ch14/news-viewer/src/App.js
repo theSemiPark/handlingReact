a17a14fc0c530c20f9742a0cf0234ab86b6419f3
theSemiPark <thisoulll@gmail.com>
2020-07-28 11:13:30 +0900

    07281113

--- ch14/news-viewer/src/App.js
+++ ch14/news-viewer/src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import NewsPage from './pages/NewsPage';
 
 const App = () => {
+  //category 뒤의 물음표는 cagegory값이 선택적이라는 뜻.
   return <Route path="/:category?" component={NewsPage} />;
 };
 
