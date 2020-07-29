9621726a8d17fcb70d5cc8a7a84630038fc9085d
theSemiPark <thisoulll@gmail.com>
2020-07-29 10:32:05 +0900

    07291032

---
+++ ch18/2/learn-redux-middleware/src/modules/sample.js
@@ -0,0 +1,49 @@
+import { createAction, handleActions } from "redux-actions";
+import { takeLatest } from "redux-saga/effects";
+import * as api from "../lib/api";
+import createRequestSaga from "../lib/createRequestSaga";
+
+// 액션 타입을 선언합니다.
+const GET_POST = "sample/GET_POST";
+const GET_POST_SUCCESS = "sample/GET_POST_SUCCESS";
+
+const GET_USERS = "sample/GET_USERS";
+const GET_USERS_SUCCESS = "sample/GET_USERS_SUCCESS";
+
+// thunk 함수를 생성합니다.
+// thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치합니다.
+
+export const getPost = createAction(GET_POST, (id) => id);
+export const getUsers = createAction(GET_USERS);
+
+// 초기 상태를 선언합니다.
+// 요청의 로딩 중 상태는 loading이라는 객체에서 관리합니다.
+
+const getPostSaga = createRequestSaga(GET_POST, api.getPost);
+const getUsersSaga = createRequestSaga(GET_USERS, api.getUsers);
+
+export function* sampleSaga() {
+  yield takeLatest(GET_POST, getPostSaga);
+  yield takeLatest(GET_USERS, getUsersSaga);
+}
+
+const initialState = {
+  post: null,
+  users: null,
+};
+
+const sample = handleActions(
+  {
+    [GET_POST_SUCCESS]: (state, action) => ({
+      ...state,
+      post: action.payload,
+    }),
+    [GET_USERS_SUCCESS]: (state, action) => ({
+      ...state,
+      users: action.payload,
+    }),
+  },
+  initialState
+);
+
+export default sample;
